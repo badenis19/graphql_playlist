772e01fdfd226d8d1093bb4db5f16c42519dde63
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-19 17:31:45 +0100

    graphql setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql'); // variable name not equal to package name just by convention
 
 // invoke the express function to create our app 
 const app = express();
+
+// route '/graphql' so that express understand we want to interact with Graphql so use graphqlHTTP 
+app.use('/graphql', graphqlHTTP({
+    // schema will tell express-graphql about the data (data-types, properties, relationships) & how the graph will look
+
+})) 
 
 app.listen(4000, () => console.log("Listening for request on port 4000"));
