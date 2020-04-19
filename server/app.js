34bf64f556e91211f45232b81b507219378551dd
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-19 18:18:53 +0100

    Root query in schema

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); // variable name not equal to package name just by convention
-
+const schema = require('./schema/schema')
 // invoke the express function to create our app 
 const app = express();
+
+
+/* MIDDLEWARE */
 
 // route '/graphql' so that express understand we want to interact with Graphql so use graphqlHTTP 
 app.use('/graphql', graphqlHTTP({
     // schema will tell express-graphql about the data (data-types, properties, relationships) & how the graph will look
-
+    schema: schema // with ES6 could only put schema as the name are the same
 })) 
 
 app.listen(4000, () => console.log("Listening for request on port 4000"));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,10 +2,10 @@ const graphql = require('graphql');
 
 // This file is to describe the schema (Object types, their relationship, how to reach into the graph to interact with data e.g. query, retrieve, mutate)
 
-const { GraphQLObjectType, GraphQLString } = graphql; // extracting GraphQLObjectType, datatypes from graphql package 
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; // extracting GraphQLObjectType, datatypes from graphql package 
 
 
-// Defining object type and data types
+// Defining Object type and data types
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -14,3 +14,30 @@ const BookType = new GraphQLObjectType({
         genre: { type: GraphQLString }
     })
 })
+
+// Defining how do we initially get to the graph to grab data 
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {  // when someone queries this BookType, expects arguments to be passed too (below) to know which book they want to query
+            type: BookType,
+            args: { id: {type: GraphQLString}}, // expect the ID to come along with the query (as in Booktype Object)
+            resolve(parent,args){ // acts after receivin query. 'parent' used with relationships | 'args' is the args key just above so 'id'
+                // code to get specific data from DB / other source
+            }
+        }
+    }
+})
+
+// Defining which query the user can use when making queries from the front-end
+module.exports = new GraphQLSchema({
+    query: RootQuery 
+})
+
+
+// const AuthorType = new GraphQLObjectType({
+//     name: 'Author',
+//     fields: () => ({
+//         id: { type: GraphQLString },
+//     })
+// })
