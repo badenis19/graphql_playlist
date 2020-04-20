7fbf76903035903a018fe53ccea66b922dd85562
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-20 07:19:03 +0100

    Type Relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,23 +6,45 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
-    GraphQLID 
+    GraphQLID, GraphQLInt
     } = graphql; // extracting GraphQLObjectType and other from graphql imported package 
 
 // dummy data TO DELETE LATER WHEN USING MONGODB
-const books = [
-    {id: '1', name: 'book1', genre: "comedy"},
-    {id: '2', name: 'book2', genre: "comedy"},
-    {id: '3', name: 'book3', genre: "thriller"}
+var books = [
+    {id: '1', name: 'book1', genre: "comedy", authorId: '1' },
+    {id: '2', name: 'book2', genre: "comedy", authorId: '3'},
+    {id: '3', name: 'book3', genre: "thriller", authorId: '2' }
 ];
 
-// Defining Object type and data types
+var authors = [
+    {id: '1', name: 'author1', age: 45},
+    {id: '2', name: 'author2', age: 43},
+    {id: '3', name: 'author3', age: 44}
+];
+
+// Defining Object (that we can query) type and data types
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author : { 
+            type: AuthorType,
+            resolve(parent,args){
+                console.log(parent);
+                return _.find(authors, {id: parent.authorId})
+            }
+        }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 })
 
@@ -35,7 +57,14 @@ const RootQuery = new GraphQLObjectType({
             args: { id: {type: GraphQLID}}, // expect the ID to come along with the query (as in Booktype Object)
             resolve(parent,args){ // acts after receivin query. 'parent' used with relationships | 'args' is the args key just above so 'id'
                 // code to get specific data from DB / other source
-                return _.find(books, {id: args.id}) // with lodash "_"
+                return _.find(books, {id: args.id}) // with lodash .find() method 
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: {type: GraphQLID}},
+            resolve(parent,args){
+                return _.find(author, {id: args.id})
             }
         }
     }
@@ -49,9 +78,3 @@ module.exports = new GraphQLSchema({ // or export default new GraphQLSchema({
 })
 
 
-// const AuthorType = new GraphQLObjectType({
-//     name: 'Author',
-//     fields: () => ({
-//         id: { type: GraphQLString },
-//     })
-// })
