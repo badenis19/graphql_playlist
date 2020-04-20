093e6d2db168214db094232cf52255a7f203f12f
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-20 13:29:39 +0100

    Create Book, stored on mongoDB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -95,6 +95,22 @@ const Mutation = new GraphQLObjectType({
                 })
                 return author.save() // to save author after creating it. IMPORTANT to write return for it to save
             }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent,args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                })
+                return book.save()
+            }
         }
     }
 })
