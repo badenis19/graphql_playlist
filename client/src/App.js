a449abc411c41e60e4f9404820607673bd0bc4eb
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-21 11:43:45 +0100

    Apollo client setup + making queries from React

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
- 
+import React, {Component} from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'; // helps react to understand Apollo
+
+// components
 import BookList from './components/BookList'
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql" // endpoint that handles queries. Apollo know we will make to this endpoint from our application
+})
 
-function App() {
-  return (
-    <div className="App">
-      <p>OOO</p>
-      <BookList/>
-    </div>
-  );
-}
+// Wrapping with AppoloProvider allow to get data from endpoint and inject data between tags.
+
+class App extends Component {
+  render(){
+    return (
+      <ApolloProvider client={client}> 
+        <div id="main">
+          <p>Bruno's Reading List</p>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    );
+}}
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}> 
+//       <div id="main">
+//         <p>Bruno's Reading List</p>
+//         <BookList />
+//       </div>
+//     </ApolloProvider>
+//   );
+// }
 
 export default App;
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost'; // package needed to parse graphql. Now we can create queries.
+import { graphql } from 'react-apollo'; // To bind Apollo with React Component
 
-function BookList() {
-  return (
-    <div>
-        <ul id="book-list">
-            <li>Book name</li>
-        </ul>
-    </div>
-  );
+
+/* Queries */
+const getBooksQuery = gql`
+{
+  books{
+    name
+    id
+  }
 }
+`;
 
-export default BookList;
+/* Component */
+
+class BookList extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+// function BookList() {
+//   console.log(this.props)
+//   return (
+//     <div>
+//       <ul id="book-list">
+//         <li>Book name</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+/* Binds the query getBooksQuery with the BookList Component using graphql() function imported.
+BookList Component now has access to the data returned from getBooksQuery query. Stored in the props */
+export default graphql(getBooksQuery)(BookList); 
