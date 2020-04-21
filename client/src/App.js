fa9e7dc869254078058998795e46d94881436030
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-21 08:40:36 +0100

    Created React app + BookList componen

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+import BookList from './components/BookList'
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>OOO</p>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
